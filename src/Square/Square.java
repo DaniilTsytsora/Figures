84833cecafa5a83f8c8afcc52935aa9fa2d41d8b
Ghosty+ <danis16119@gmail.com>
2024-03-08 15:37:52 +0300

    update

--- src/Square/Square.java
+++ src/Square/Square.java
@@ -5,13 +5,7 @@ public class Square extends Form {
     public Square(double side) {
         this.side=side;
     }
-    public double getSide() {
-        return side;
-    }
-    public void setSide(double side) {
-        this.side=side;
 
-    }
 
     @Override
     public double getArea() {
